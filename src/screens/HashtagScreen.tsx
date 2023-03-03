2bbc2d974e0bd6328bb91db07eba5f7518b46422
Osongmin <autumninmoon@gmail.com>
2023-03-03 22:22:53 +0900

    refactor: HashtagScreen.tsx Text 문구 변경

--- src/screens/HashtagScreen.tsx
+++ src/screens/HashtagScreen.tsx
@@ -6,7 +6,7 @@ export const HashtagScreen: React.FC = () => {
   return (
     <View>
       <MoimHeader showBackButton={true} />
-      <Text>헤시태그 스크린입니다.</Text>
+      <Text>헤시태그 스크린입니다</Text>
     </View>
   );
 };
