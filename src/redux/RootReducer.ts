5a4ef57a7437f6c4503983f7fc2d60cbe93db8d3
IAN <ian@IANui-MacBookAir.local>
2023-03-15 22:30:48 +0900

    connect reducer and slice and action

--- src/redux/RootReducer.ts
+++ src/redux/RootReducer.ts
@@ -2,11 +2,15 @@ import { combineReducers } from "redux";
 import { EventSlice } from "./Slices/Event";
 import { postEventSlice } from "./Slices/EventPost";
 import { GlobalSlice } from "./Slices/Global";
+import { ProfileSlice } from "./Slices/Profile"
+import { ProfilePostSlice } from "./Slices/ProfilePost";
 
 export const rootReducer = combineReducers({
   eventPost: postEventSlice.reducer,
   event: EventSlice.reducer,
   global: GlobalSlice.reducer,
+  profile: ProfileSlice.reducer,
+  profilePost: ProfilePostSlice.reducer,
 });
 
 export type RootState = ReturnType<typeof rootReducer>;
