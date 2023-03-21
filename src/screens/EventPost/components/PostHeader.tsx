b3933ea79693b2dd2e26b7dd482670d8af8ee5b3
9utty <leegh4250@gmail.com>
2023-03-21 21:07:26 +0900

    feat: 현재는 서버와 연결했음... 파티참여, 참여취소가 지금 현재 테스트가 안됨...

--- src/screens/EventPost/components/PostHeader.tsx
+++ src/screens/EventPost/components/PostHeader.tsx
@@ -13,10 +13,17 @@ import {
 import { styleHeader } from "../styleSheets/PostHeader";
 import { HomeStackParam } from "../../../navigations/HomeNavigation";
 import { useAppDispatch } from "../../../redux/RootStore";
-import { postEventSlice } from "../../../redux/Slices/EventPost";
+import { EventPostDto, postEventSlice } from "../../../redux/Slices/EventPost";
 import { RootState } from "../../../redux/RootReducer";
 import { UISlice } from "../../../redux/Slices/UI";
 import { key } from "../../../../config";
+import instance from "../../../utils/axios";
+import { GlobalSlice } from "../../../redux/Slices/Global";
+import { EventDto, EventSlice } from "../../../redux/Slices/Event";
+import { hashtagType } from "../../../@Type/hashtag";
+import { setUseProxies } from "immer";
+import { PostEventScreen } from "../PostEventScreen";
+import { EventHeader } from "src/screens/Event/components/Header";
 
 const wp = wpSize("100%");
 const hp = hpSize("100%");
@@ -39,59 +46,76 @@ export const PostHeader: React.FC = () => {
 
   useFocusEffect(
     React.useCallback(() => {
-      setImageBinary(null);
+      // setImageBinary(null);
       return () => {};
     }, [])
   );
 
-  const ImageConvert = (images: string[]) => {
-    const imageConvertTobinary = (blobUrl: Blob) => {
+  const imageConvertTobinary = useCallback(async (blobUrl: Blob) => {
+    return new Promise<string>((resolve, reject) => {
       const fileRef = new FileReader();
-      const testDate = fileRef.readAsDataURL(blobUrl);
       try {
         fileRef.onload = () => {
           const testDate = fileRef.result;
-          setImageBinary(testDate);
+          resolve(testDate as string);
         };
+        fileRef.onerror = (error) => {
+          reject(error);
+        };
+        fileRef.readAsDataURL(blobUrl);
       } catch (error) {
         console.error(error);
+        reject(error);
       }
-      return ImageBinary;
-    };
+    });
+  }, []);
 
-    const postUploadImage = async (image: string): Promise<void> => {
+  const postUploadImage = useCallback(
+    async (image: string): Promise<string | void> => {
       try {
         const imageUrl = await fetch(image);
         const blobUrl = await imageUrl.blob();
-        const dateBin = imageConvertTobinary(blobUrl);
+        const dateBin = await imageConvertTobinary(blobUrl);
         if (!dateBin) {
+          console.log("리턴!");
           return;
         }
-        const date = Date.now();
-        const bolbId = blobUrl._data.blobId;
         const response = await axios.post(key.IMAGE_UPLOAD_URL, {
           image: dateBin.slice(23),
-          imageId: bolbId,
+          imageId: blobUrl._data.blobId,
           userId: globalState.userId <= 0 ? 12341234 : globalState.userId,
         });
         const responseObj = JSON.parse(response.request._response);
-        dispatch(
-          postEventSlice.actions.addImages([
-            ...event.EventDto.eventImages,
-            responseObj.url,
-          ])
-        );
+        return responseObj.url;
       } catch (error) {
         console.log(error);
       }
-    };
-    if (!images) return [];
-    for (let i = 0; i < images.length; ++i) {
-      postUploadImage(images[i]);
-    }
-  };
+    },
+    []
+  );
 
-  const handleBackButton = () => {
+  const ImageConvert = useCallback(
+    async (images: string[]) => {
+      if (!images) return [];
+
+      const urls: string[] = [];
+      const promises: Promise<string | void>[] = [];
+      for (let i = 0; i < images.length; ++i) {
+        const promise: Promise<string | void> = postUploadImage(images[i]);
+        promises.push(promise);
+      }
+
+      const responses: (string | void)[] = await Promise.all(promises);
+      for (const response of responses) {
+        if (response) urls.push(response);
+      }
+
+      return urls; // 필요하면 반환값으로 URL 배열을 반환할 수 있습니다.
+    },
+    [dispatch]
+  );
+
+  const handleBackButton = useCallback(() => {
     Alert.alert(
       "",
       "작성을 취소하시겠습니까?",
@@ -112,7 +136,7 @@ export const PostHeader: React.FC = () => {
       { cancelable: false }
     );
     return true;
-  };
+  }, [navigation]);
 
   useEffect(() => {
     BackHandler.addEventListener("hardwareBackPress", handleBackButton);
@@ -132,39 +156,60 @@ export const PostHeader: React.FC = () => {
         },
         {
           text: "Yes",
-          onPress: () => {
+          onPress: async () => {
             if (
-              event.EventDto.eventTitle.length <= 0 && IsUpdate === true
+              event.EventDto.header.length <= 0 && IsUpdate === true
                 ? event.eventImageCount <= 0 &&
                   event.eventSelectImage.length <= 0
-                : event.EventDto.eventImages.length <= 0 &&
-                  event.EventDto.eventHashtagId <= 0 &&
-                  event.EventDto.eventParticipant <= 0 &&
-                  event.EventDto.eventMap.address.length <= 0 &&
+                : event.EventDto.images.length <= 0 &&
+                  event.EventDto.hashtag <= 0 &&
+                  event.EventDto.maxParticipant <= 0 &&
+                  event.EventDto.location.length <= 0 &&
                   event.EventDto.eventDate.length <= 0
             ) {
               Alert.alert("", "필수항목을 입력해주세요.");
               return;
             }
+            let uploadedImageUrls: string[];
             if (IsUpdate === true) {
               //TODO : 이미지 요청을 selectImage 껄로만 해서 서버에 저장 후 데이터 파싱해야함
               const removedImages: string[] = [...event.removeImages];
               // TODO: removedImages 에 있는 애들을 delete요청
-              const images: string[] = [...event.EventDto.eventImages];
+              const images: string[] = [...event.EventDto.images];
               const removeImage: string[] = images.filter(
                 (image: string) => !removedImages.includes(image)
               );
               dispatch(postEventSlice.actions.addImages(removeImage));
               const tmpImage: string[] = [...event.eventSelectImage];
-              ImageConvert(tmpImage);
-              dispatch(UISlice.actions.setEventUpdate(false));
-            } else if (IsUpdate === false) {
-              const tmpImage: string[] = [...event.EventDto.eventImages];
-              ImageConvert(tmpImage);
+              uploadedImageUrls = await ImageConvert(tmpImage);
+            } else {
+              const tmpImage: string[] = [...event.EventDto.images];
+              uploadedImageUrls = await ImageConvert(tmpImage);
             }
             // TODO: 여기가 곧 모든걸 파싱하고 폼을 완성해서 서버로 보내는 곳
-            handleDeleteAll();
-            navigation.navigate("Event");
+            try {
+              const req: EventPostDto = {
+                header: event.EventDto.header,
+                content: event.EventDto.content,
+                openTalkLink: event.EventDto.openTalkLink,
+                hashtag: event.EventDto.hashtag,
+                eventDate: event.EventDto.eventDate,
+                images: uploadedImageUrls,
+                tradeName: event.EventDto.tradeName,
+                location: event.EventDto.location,
+                longitude: event.EventDto.longitude,
+                latitude: event.EventDto.latitude,
+                maxParticipant: event.EventDto.maxParticipant,
+              };
+              const response = await instance.post(`${key.URL}event`, req); // 요청 URL과 데이터를 전달합니다.
+              const data = await response.data;
+              dispatch(UISlice.actions.setSelectEventId(data.data));
+              navigation.navigate("Event");
+              handleDeleteAll();
+            } catch (error) {
+              console.error("Error in post request:", error);
+              // 에러 처리를 원하는 방식으로 수정
+            }
           },
         },
       ],
