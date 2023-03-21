f45820e9867ed98eaba6611e6b454d610e2663d3
9utty <leegh4250@gmail.com>
2023-03-21 21:07:45 +0900

    feat: 굿굿

--- src/utils/axios.ts
+++ src/utils/axios.ts
@@ -5,6 +5,7 @@ import { RootState } from "../redux/RootReducer";
 import { key } from "../../config";
 import { useAppDispatch } from "../redux/RootStore";
 import { GlobalSlice } from "../redux/Slices/Global";
+import store from "../redux/RootStore";
 
 interface AuthError extends Error {
   response?: {
@@ -18,27 +19,25 @@ const instance = axios.create({
   timeout: 10000, // 타임아웃 설정, 10초 내에 응답이 없으면 에러 처리
 });
 
-// // 인터셉터 추가(헤더에 token 붙혀서 보내기) ps - 지금 적용하면 서버하고 통신못할수도
-// instance.interceptors.request.use(
-//   async (config) => {
-//     const accessToken = useSelector(
-//       (state: RootState) => state.global.AccessToken
-//     );
-//     if (accessToken) {
-//       config.headers.Authorization = `Bearer ${accessToken}`;
-//     }
-//     return config;
-//   },
-//   (error) => {
-//     return Promise.reject(error);
-//   }
-// );
+// 인터셉터 추가(헤더에 token 붙혀서 보내기) ps - 지금 적용하면 서버하고 통신못할수도
+instance.interceptors.request.use(
+  async (config) => {
+    const accessToken = store.getState().global.AccessToken;
+    if (accessToken) {
+      config.headers.Authorization = `Bearer ${accessToken}`;
+    }
+    return config;
+  },
+  (error) => {
+    return Promise.reject(error);
+  }
+);
 
 instance.interceptors.response.use(
-  (response) => response, // 2xx status 코드인 경우 그대로 반환
+  (response) => {
+    return response;
+  }, // 2xx status 코드인 경우 그대로 반환
   async (error) => {
-    const global = useSelector((state: RootState) => state.global);
-    const dispatch = useAppDispatch();
     const originalRequest = error.config;
     if (error.response && error.response.status === 401) {
       // 만약 401 에러가 발생한 경우
@@ -46,12 +45,14 @@ instance.interceptors.response.use(
         // 요청을 재시도 하지 않은 경우
         originalRequest._retry = true;
         try {
-          const response = await axios.post(`${URL}/auth/refresh`, {
+          const acToken = store.getState().global.AccessToken;
+          const response = await axios.post(`${key.URL}auth/refresh`, {
+            accessToken: acToken, // 시크릿저장소에서 refreshToken을 가져옴
             refreshToken: await SecureStore.getItemAsync("refreshToken"), // 시크릿저장소에서 refreshToken을 가져옴
           });
           const { accessToken, refreshToken } = response.data;
           // 새로운 accessToken, refreshToken을 받아서 저장
-          dispatch(GlobalSlice.actions.addAToken(accessToken));
+          store.dispatch(GlobalSlice.actions.addAToken(accessToken));
           await SecureStore.setItemAsync("refreshToken", refreshToken); // 시크릿저장소에 refreshToken을 저장
           // 기존 요청의 Authorization 헤더에 새로운 accessToken을 담아서 재요청
           // 유저 아이디를 받는지 모르겠음.. 받는데 어떻게 받는지 모름..
