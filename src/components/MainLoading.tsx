29bd2135dbd5bf3895178507ad964983e7a0572b
9utty <leegh4250@gmail.com>
2023-03-29 21:01:35 +0900

    feat: 머지 후 수정

---
+++ src/components/MainLoading.tsx
@@ -0,0 +1,32 @@
+import { StyleSheet, Text, View, Image } from "react-native";
+import React from "react";
+import {
+  widthPercentageToDP as wpSize,
+  heightPercentageToDP as hpSize,
+} from "react-native-responsive-screen";
+
+const wp = wpSize("100%");
+const hp = hpSize("100%");
+
+const MainLoading: React.FC = () => {
+  return (
+    <View style={styles.container}>
+      <Image source={require("../assets/logo.png")} />
+    </View>
+  );
+};
+
+export default MainLoading;
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: "center",
+    alignContent: "center",
+    alignItems: "center",
+  },
+  image: {
+    width: hp * 0.2,
+    height: hp * 0.2,
+  },
+});
