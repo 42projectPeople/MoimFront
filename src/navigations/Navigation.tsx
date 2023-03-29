9aa327a0c737c2d3d7e453d6962ba651c33aadfa
9utty <leegh4250@gmail.com>
2023-03-29 20:13:48 +0900

    refactor: 머지 후 수정

--- src/navigations/Navigation.tsx
+++ src/navigations/Navigation.tsx
@@ -1,15 +1,15 @@
-import { useNavigation, useRoute, RouteProp} from "@react-navigation/native";
-import { HomeStackParam } from './HomeNavigation'
+import { useNavigation, useRoute, RouteProp } from "@react-navigation/native";
+import { HomeStackParam } from "./HomeNavigation";
 import {
-	createNativeStackNavigator,
-	NativeStackNavigationProp,
-  } from "@react-navigation/native-stack";
+  createNativeStackNavigator,
+  NativeStackNavigationProp,
+} from "@react-navigation/native-stack";
 
 export const useHomeNavigation = <RouteName extends keyof HomeStackParam>() => {
-	return useNavigation<NativeStackNavigationProp<HomeStackParam, RouteName>>();
-  };
-  
-  export const useRouteProps = <RouteName extends keyof HomeStackParam>() => {
-	  type props = RouteProp<HomeStackParam, RouteName>;
-	  return useRoute<props>();
-  }
+  return useNavigation<NativeStackNavigationProp<HomeStackParam, RouteName>>();
+};
+
+export const useRouteProps = <RouteName extends keyof HomeStackParam>() => {
+  type props = RouteProp<HomeStackParam, RouteName>;
+  return useRoute<props>();
+};
